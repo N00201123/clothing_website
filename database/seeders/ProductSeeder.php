ff6f88be0732d1a5775a75705af198a4ea501bde
Aaron Saula <n00201123@iadt.ie>
2023-03-29 17:13:19 +0100

    SeederUpdated

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -16,5 +16,15 @@ class ProductSeeder extends Seeder
     public function run()
     {
         Product::factory()->times(40)->create();
+
+        // $product1 = new Product();
+        // $product1->title = "Tommy Hilfiger";
+        // $product1->description = "Black Heavyweight Hoodie";
+        // $product1->date = "2022-01-28";
+        // $product1->price = "19.99";
+        // $product1->size = "M";
+        // $product1-> type = "Hoodie";
+        // $product1->image = "Clothes_pt1.png";
+        // $product1->save();
     }
 }
