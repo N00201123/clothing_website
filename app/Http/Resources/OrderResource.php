1b74f6ac3e2caaf01c116eccc09aa6a0e652f080
Aaron Saula <n00201123@iadt.ie>
2023-04-25 15:49:19 +0100

    Order_Product_Resource

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -14,11 +14,17 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
+        $products = array();
+        foreach ($this->products as $product) {
+        echo $product->title;
+            array_push($products, $product->title);
+        }
         return [
             'id' => $this->id,
             'date' => $this->date,
             'shipping_price' => $this->shipping_price,
-            'customer_id' => $this->customer_id
+            'customer_id' => $this->customer_id,
+            'products' => $products
         ];
     }
 }
